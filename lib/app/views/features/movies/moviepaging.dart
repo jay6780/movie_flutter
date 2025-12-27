d5394fa4963b13cd631d8306032a00e1d84a9e25
Ar-jay <arjayurbina43@gmail.com>
2025-12-28 01:53:03 +0800

    forgot

--- lib/app/views/features/movies/moviepaging.dart
+++ lib/app/views/features/movies/moviepaging.dart
@@ -1,12 +1,12 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:free_movie/app/model/movie.dart';
 import 'package:free_movie/app/utils/app_colors.dart';
 import 'package:free_movie/bloc/movie_pagination/moviepagination_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:shimmer/shimmer.dart';
 
